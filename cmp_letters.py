80f616da5916743c75adc1b9e0bf8badec440771
Don Cross <cosinekitty@gmail.com>
2020-04-25 15:49:21 -0400

    Factored out custom base64 encoder.

--- cmp_letters.py
+++ cmp_letters.py
@@ -1,4 +1,5 @@
 from huffman import HuffmanEncoder
+from binary_base64 import Base64
 
 class Compressor:
     r'''Strategy:
@@ -13,7 +14,7 @@ class Compressor:
         bits = self._Encode(words, charCode)
         source = "Char=" + charRoot.SourceCode() + "\n"
         source += "NumChars={:d}\n".format(sum(1+len(w) for w in words)-1)
-        source += "Bits=r'''\n" + self._Base64(bits) + "'''\n"
+        source += "Bits=r'''\n" + Base64(bits) + "'''\n"
         return source
 
     def _Encode(self, words, charCode):
@@ -22,37 +23,6 @@ class Compressor:
             bits += ''.join(charCode[c] for c in w)
             bits += charCode['\n']
         return bits
-
-    def _Base64(self, bits):
-        # Encode the 'bits' string, that contains a sequence of '0' and '1' chars,
-        # into base64. Each base64 output character represents 6 bits, because 2**6 == 64.
-        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-        text = ''
-        lineLength = 0
-        pos = 0
-        nbits = len(bits)
-        while pos < nbits:
-            # Remove the next 6 bits.
-            # If there are fewer than 6 bits remaining, pad with '0' on the end.
-            if pos + 6 > nbits:
-                chunk = bits[pos:]
-                while len(chunk) < 6:
-                    chunk += '0'
-            else:
-                chunk = bits[pos:6+pos]
-            pos += 6
-            # Compute the integer index from 'chunk'
-            index = 0
-            for b in chunk:
-                index <<= 1
-                if b == '1':
-                    index |= 1
-            text += alphabet[index]
-            lineLength += 1
-            if lineLength == 80:
-                text += '\n'
-                lineLength = 0
-        return text
 
     def _HuffmanCode(self, words):
         charHuff = HuffmanEncoder()
--- cmp_prefix.py
+++ cmp_prefix.py
@@ -1,4 +1,5 @@
 from huffman import HuffmanEncoder
+from binary_base64 import Base64
 
 class Compressor:
     r'''Strategy:
@@ -22,7 +23,7 @@ class Compressor:
         source += "Tail=" + tailRoot.SourceCode() + "\n"
         source += "Char=" + charRoot.SourceCode() + "\n"
         source += "NumWords={:d}\n".format(len(words))
-        source += "Bits=r'''\n" + self._Base64(bits) + "'''\n"
+        source += "Bits=r'''\n" + Base64(bits) + "'''\n"
         return source
 
     def _Encode(self, words, repeatCode, tailCode, charCode):
@@ -37,37 +38,6 @@ class Compressor:
                 bits += charCode[c]
             pw = w
         return bits
-
-    def _Base64(self, bits):
-        # Encode the 'bits' string, that contains a sequence of '0' and '1' chars,
-        # into base64. Each base64 output character represents 6 bits, because 2**6 == 64.
-        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-        text = ''
-        lineLength = 0
-        pos = 0
-        nbits = len(bits)
-        while pos < nbits:
-            # Remove the next 6 bits.
-            # If there are fewer than 6 bits remaining, pad with '0' on the end.
-            if pos + 6 > nbits:
-                chunk = bits[pos:]
-                while len(chunk) < 6:
-                    chunk += '0'
-            else:
-                chunk = bits[pos:6+pos]
-            pos += 6
-            # Compute the integer index from 'chunk'
-            index = 0
-            for b in chunk:
-                index <<= 1
-                if b == '1':
-                    index |= 1
-            text += alphabet[index]
-            lineLength += 1
-            if lineLength == 80:
-                text += '\n'
-                lineLength = 0
-        return text
 
     def _HuffmanCodes(self, words):
         repeatHuff = HuffmanEncoder()
